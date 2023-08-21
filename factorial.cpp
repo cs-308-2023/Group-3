8eb91976fb2c45712728a9c474f42085eeb52a5c
rohan <rohan@gmail.com>
2023-08-21 15:30:43 +0530

    negative Factorial Fixed

--- factorial.cpp
+++ factorial.cpp
@@ -2,7 +2,9 @@
 // factorial function
 int factorial(int n){
 	if(n<0){
-		cout<<"Factorial not defined for negative number"<<endl;
+		cout<<"Factorial is not defined for negative number"<<endl;
+		return -1;
+		
 	}
 	else{
 	    if(n!=1){

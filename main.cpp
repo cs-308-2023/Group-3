2416f7d2c00807da2e00ac4be7262a73582470d1
rohan <rohan@gmail.com>
2023-08-27 17:31:25 +0530

    Added GCD print statement in main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,9 @@ int main(){
     print_hello();
     cout<< endl;
     cout<<"The factorial of 5 is "<< factorial(5)<< endl;
+    int x = 56;
+    int y = 98;
+    cout<<"The GCD of "<<x<<" and  "<<y<<" is "<<gcd(x,y)<<endl;
+    cout<<"The GCD of "<<x<<" and  "<<y<<" using recursion is "<<gcd_recursive(x,y)<<endl;
     return 0;
 }
